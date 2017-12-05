ae09a0deea95d0129e0fa49afe77ca85588950cd
Christina <github email address>
2017-12-04 20:18:56 -0700

    Made changes to projects controller file to make the routes work for edit, index and projects views. Left off with the id/edit route and view to work on tomorrow.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,15 @@
 class ProjectsController < ApplicationController
 
-  get '/projects' do  #This works
+  get '/projects' do
     if logged_in?
-      @project = Project.all
+      @projects = Project.all
       erb :'projects/index'
     else
       redirect to '/login'
     end
   end
 
-  get '/projects/new' do  #this works
+  get '/projects/new' do
     if logged_in?
       erb :'projects/new'
     else
@@ -18,16 +18,16 @@ class ProjectsController < ApplicationController
   end
 
   post '/projects' do
-    if params[:name] == "" || params[:directions] == ""
+    if params[:directions] == ""
       redirect to "/projects/new"
     else
-      @project = current_user.projects.create(:name => params[:name], :directions => params[:directions])
-      redirect to "/projects/projects/#{@project.id}"
+      @project = current_user.projects.create(:name => params[:name], :directions => params[:directions], :material_id => params[:material_id])
+      redirect to "/projects/#{@project.id}"
     end
   end
 
-  get 'projects/:id' do
-    if !logged_in?
+  get '/projects/:id' do
+    if logged_in?
       @project = Project.find_by_id(params[:id])
       erb :'projects/projects'
     else
@@ -35,7 +35,7 @@ class ProjectsController < ApplicationController
     end
   end
 
-  get '/projects/:id/edit' do
+  get '/projects/:id/edit' do   #*******Stopped at this point to figure out projects.erb form
     if logged_in?
       @project = Project.find_by(params[:id])
       if current_user == @project.user
@@ -48,23 +48,85 @@ class ProjectsController < ApplicationController
     end
   end
 
-    patch '/projects/:id' do
-      @project = Project.find_by(id: params[:id])
-      @project.directions = [:directions]
 
-      if @project.save
-        redirect to "/projects/#{@project.id}"
-      else
-        redirect to "/projects/#{@project.id}/edit"
-      end
-    end
 
-    delete '/projects/:id/delete' do
-      @project = Project.find_by(params[:id])
-      if current_user == @project.user
-        @project.delete
-      end
-      redirect to '/projects'
-    end
+
+#  get '/projects/:id/:slug' do
+#    if logged_in?
+#      @user = current_user
+#      @project = Project.find_by(params[:user_id])
+#      erb :'/projects/edit'
+#    end
+#  end
+
+  #get '/projects/new' do  #this works
+  #  if logged_in?
+  #    erb :'projects/new'
+  #  else
+#      redirect to '/login'
+#    end
+#  end
+
+
+
+  #get '/projects/:id/:slug/edit' do
+#
+  #  if logged_in?
+  #    @project = Project.find_by(params[:id])
+  #    if current_user == @project.users
+  #      erb :'projects/edit'
+  #    else
+  #      redirect to '/projects'
+  #    end
+  #  else
+  #    redirect to '/login'
+  #  end
+  #end
+
+
+
+#  post '/projects/:id' do
+#    @project = Project.find(params[:id])
+#    @project.directions = [:directions]
+#
+#    if @project.save
+#      redirect to "/projects/#{@project.id}"
+#    else
+#      redirect to "/projects/#{@project.id}/edit"
+#    end
+#  end
+
+#  get '/projects/:id/edit' do
+#    if logged_in?
+#      @project = Project.find(params[:id])
+#      erb :'projects/edit'
+#    end
+#  end
+
+#  post '/projects' do  #this works
+#    if params[:name] == "" || params[:directions] == ""
+#      redirect to "/projects/new"
+#    else
+#      @project = current_user.projects.create(:name => params[:name], :directions => params[:directions])
+#      redirect to "/projects/#{@project.id}"
+#    end
+#  end
+
+#  get '/projects/:id/:slug' do
+#    if !logged_in?
+#      @project = Project.find_by_id(params[:id])
+#      erb :'projects'
+#    else
+#      redirect to '/login'
+#    end
+#  end
+
+#    delete '/projects/:id/delete' do
+#      @project = Project.find_by(params[:id])
+#      if current_user == @project.user
+#        @project.delete
+#      end
+#      redirect to '/projects'
+#    end
 
 end
